5785c4c4cbd579233be09448fc4884b930167338
Adriano Goncalves <amg1976@gmail.com>
2018-04-12 19:37:24 +0100

    Initial Commit

---
+++ IrishRailRealtime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IrishRailRealtime
+//
+//  Created by Adriano Goncalves on 12/04/2018.
+//  Copyright © 2018 Adriano Goncalves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
